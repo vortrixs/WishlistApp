2e9d6962ec1a5fc8eaf6e6c5d6b30edb7aec69da
Hans Erik Jepsen <hanserikjepsen@hotmail.com>
2019-11-11 17:32:01 +0100

    Change negative priority to 0

--- WishlistApp/Wish.cs
+++ WishlistApp/Wish.cs
@@ -9,13 +9,13 @@
         public int Priority {
             get { return priority; } 
             private set {
-                if (0 == value)
+                if (0 >= value)
                 {
                     value = 1;
                 }
 
                 priority = value;
-            } 
+            }
         }
 
         public Wish(string name, int priority)
